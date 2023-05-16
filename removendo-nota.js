6f00441c665e520d4489befe64bd07a033e13018
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-16 14:01:40 -0300

    Método pop

--- removendo-nota.js
+++ removendo-nota.js
@@ -7,3 +7,5 @@ console.log(notas);
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(`A média é ${media}.`);
+
+// O método pop() remove o último elemento de um array e retorna aquele elemento.
