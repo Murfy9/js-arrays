b1c6b45d553f5522749c1443a64aecda59c7e104
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-16 14:14:25 -0300

    Método splice

---
+++ atualizando-lista.js
@@ -0,0 +1,7 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+nomes.splice(1, 2, "Rodrigo");
+
+console.log(nomes);
+
+// O método splice() altera o conteúdo de uma lista, adicionando novos elementos enquanto remove elementos antigos.
