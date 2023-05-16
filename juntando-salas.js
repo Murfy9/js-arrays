0731a5f682e4f4a1820f89c9bb2d91317c9fc81f
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-16 14:20:27 -0300

    Método concat

---
+++ juntando-salas.js
@@ -0,0 +1,8 @@
+const salaJS = ["Evaldo", "Camis", "Mari"];
+const salaPython = ["Ju", "Leo", "Raquel"];
+
+const salasUnificadas = salaJS.concat(salaPython);
+
+console.log(salasUnificadas);
+
+// O método concat() é utilizado para mesclar dois ou mais arrays. Esse método não altera os arrays existentes, mas, em vez disso, retorna um novo array.
