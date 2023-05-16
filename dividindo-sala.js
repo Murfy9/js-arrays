5b15e2f476b9eb8dd8d105123de7090df0a526d6
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-16 13:59:06 -0300

    Método slice

--- dividindo-sala.js
+++ dividindo-sala.js
@@ -26,3 +26,5 @@ const sala2 = alunos.slice(alunos.length / 2);
 
 console.log(sala1);
 console.log(sala2);
+
+// O método slice() retorna uma cópia de parte de um array a partir de um subarray criado entre as posições início e fim (fim não é necessário) de um array original. O array original não é modificado.
