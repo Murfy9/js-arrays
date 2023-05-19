bd67a37eba82b9dbf1adb50a561c9f880bf9c45f
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-19 14:17:35 -0300

    Spread operator

---
+++ copiando-array.js
@@ -0,0 +1,8 @@
+const notas = [7, 7, 8, 9];
+
+const novasNotas = [...notas, 10];
+
+console.log(`As novas notas são ${novasNotas}`);
+console.log(`As notas originais são ${notas}`);
+
+// Spread operator "...", ou "operador de espalhamento". Ao utilizá-lo junto com uma lista, ele pega todo o conteúdo desse array e espalhar seus valores no local que estamos indicando.
